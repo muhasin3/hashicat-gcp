240580a59c6380e5c75191855a2d768017656a45
Muhasin Edathil Meethal <muhasin3@gmail.com>
2021-05-20 04:40:25 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,16 @@
-subnets = [
+module "network" {
+  source  = "app.terraform.io/muhasin-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "muhasin-network"
+  project_id = var.project
+
+  subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "muhasin-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+
+}
